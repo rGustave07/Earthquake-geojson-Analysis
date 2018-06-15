c1b2a7604524aa234e440375056e3fe3459febf5
Ritter Gustave <Gustaver@uci.edu>
2018-06-15 11:12:47 -0700

    Skeleton done

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,8 +2,8 @@ window.onload = () => {
   console.log("Systems operational");
 
   let dataMap = L.map("map", {
-    center: [45.52, -122.67],
-    zoom: 13
+    center: [39.8283, -98.5795],
+    zoom: 5
   });
 
   L.tileLayer(
@@ -11,4 +11,25 @@ window.onload = () => {
     "access_token=pk.eyJ1Ijoia29yYmVuaWsiLCJhIjoiY2poeG5idDJqMGFkdTNwcXNzMDNpeHQ4OCJ9.-IvDdr4zgTcZeqS7HDkK8A"
   ).addTo(dataMap);
 
-}
+  let queryurl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+  d3.json(queryurl, response => {
+    locations = response.features;
+    // console.log(locations);
+    let markers = L.markerClusterGroup();
+    console.log(locations[0]);
+    locations.forEach( eqPoint => {
+      coordsMetaInfo = eqPoint.properties
+      coordsCurrentEqpoint = eqPoint.geometry.coordinates;
+      // console.log(coordsCurrentEqpoint);
+      cleanedCoords = [coordsCurrentEqpoint[1], coordsCurrentEqpoint[0]];
+      // console.log(cleanedCoords);
+
+      if (cleanedCoords) {
+        let descriptor =
+        markers.addLayer(L.marker(cleanedCoords).bindPopup(`<h1>Hit Point</h1> <hr> <p>${coordsMetaInfo.place}</p>`));
+      };
+    });
+    dataMap.addLayer(markers)
+
+  });
+};
